e7a70e076e1a14bdc47b4252cb2e31cd4e5ea37e
midhul-centrric <midhul.intern@centrric.in>
2026-03-26 12:33:57 +0530

    fix: export Waste Type and other custom doctypes to standard app files

---
+++ enviro/enviro/doctype/waste_type/waste_type.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Centrric Innovations and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Waste Type", {
+// 	refresh(frm) {
+
+// 	},
+// });
