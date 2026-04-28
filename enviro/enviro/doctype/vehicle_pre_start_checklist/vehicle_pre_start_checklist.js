850a18d2122afb4c4d2fbb8d7c3766c4ad95fbbc
midhul-centrric <midhul.intern@centrric.in>
2026-04-28 15:50:07 +0530

    chore: wipe out deprecated Vehicle Pre-Start Checklist doctype

--- enviro/enviro/doctype/vehicle_pre_start_checklist/vehicle_pre_start_checklist.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2026, Centrric Innovations and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Vehicle Pre-Start Checklist", {
-// 	refresh(frm) {
-
-// 	},
-// });
