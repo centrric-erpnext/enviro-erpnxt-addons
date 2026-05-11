5b883c9a247be78dbfa8b8a40e62391b48e59fc3
midhul-centrric <midhul.intern@centrric.in>
2026-05-11 12:29:37 +0530

    feat: add read-only Waste Type field to Enviro Job Card

--- enviro/enviro/doctype/enviro_job_card/enviro_job_card.js
+++ enviro/enviro/doctype/enviro_job_card/enviro_job_card.js
@@ -30,4 +30,26 @@ frappe.ui.form.on("Enviro Job Card", {
 			});
 		}
 	},
+	source_quotation: function (frm) {
+		if (frm.doc.source_quotation) {
+			frappe.call({
+				method: "frappe.client.get_list",
+				args: {
+					doctype: "Quotation Item",
+					filters: { parent: frm.doc.source_quotation },
+					fields: ["custom_waste_type"],
+				},
+				callback: function (r) {
+					if (r.message) {
+						let types = [
+							...new Set(r.message.map((i) => i.custom_waste_type).filter(Boolean)),
+						];
+						frm.set_value("custom_waste_type", types.join(", "));
+					}
+				},
+			});
+		} else {
+			frm.set_value("custom_waste_type", "");
+		}
+	},
 });
--- enviro/enviro/doctype/enviro_job_card/enviro_job_card.py
+++ enviro/enviro/doctype/enviro_job_card/enviro_job_card.py
@@ -6,6 +6,23 @@ from frappe.model.document import Document
 
 
 class EnviroJobCard(Document):
+	def validate(self):
+		self.fetch_waste_type()
+
+	def fetch_waste_type(self):
+		"""Aggregates all unique waste types from the linked quotation's items."""
+		if self.source_quotation:
+			waste_types = frappe.get_all(
+				"Quotation Item",
+				filters={"parent": self.source_quotation, "custom_waste_type": ["is", "set"]},
+				pluck="custom_waste_type",
+			)
+			if waste_types:
+				# Store as a comma-separated string
+				self.custom_waste_type = ", ".join(sorted(list(set(waste_types))))
+			else:
+				self.custom_waste_type = ""
+
 	def after_insert(self):
 		# Automatically link this newly generated Job Card back into the Source Quotation
 		if self.source_quotation:
